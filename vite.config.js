3ae00e2e1c723c5b60f75960bda6b404a7540bb0
Alisina Faramarzi <alisina.faramarzi@substorm.ai>
2025-05-24 16:41:06 +0200

    small fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sina3131/', // Replace with your actual repo name
+  base: '/portfolio/', 
   plugins: [react()]
 });
