e7fe941e6e7410b88ea9062425cd0afe6df061b6
Alisina Faramarzi <alisina.faramarzi@substorm.ai>
2025-05-29 15:37:09 +0200

    ui changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,16 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-5 h-5 rounded-full bg-[#7f817f]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Alisina</span>
+            Hi, I'm <span className='text-[#949295]'>Alisina</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop AI and machine learning application <br className='sm:block hidden' />
+            I develop AI and machine learning <br className='sm:block hidden' />
             applications
           </p>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Alisina &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            <span className='sm:block hidden'> | AI and machine learning developer</span>
           </p>
         </Link>
 
