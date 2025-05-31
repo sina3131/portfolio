90216e6317f6a23563bd89e120b14835c101791b
Alisina Faramarzi <alisina.faramarzi@substorm.ai>
2025-05-31 18:32:00 +0200

    added contact part

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,6 +26,7 @@ const Contact = () => {
       [name]: value,
     });
   };
+  console.log("Template ID:", import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,9 +38,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "AI and machine learning developer",
+          to_name: "Alisina faramarzi",
           from_email: form.email,
-          to_email: "sina.313@yahoo.com",
+          to_email: "sa1212112121@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -72,7 +73,7 @@ const Contact = () => {
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
-        <p className={styles.sectionSubText}>Get in touch</p>
+        <p className={styles.sectionSubText}>Let’s make internet magic together.” ✨🧙‍♂️</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
         <form
@@ -87,7 +88,7 @@ const Contact = () => {
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
@@ -98,7 +99,7 @@ const Contact = () => {
               name='email'
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
@@ -109,7 +110,7 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
+              placeholder='Drop your message below—I’ll get back to you shortly.'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
